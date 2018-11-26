5539aa217d9b801cd03642575895b761ba651b87
Jussi Liukkonen <jussi.liukkonen@fmi.fi>
2018-11-26 15:12:37 +0200

    LAYERS-94 fix

--- src/view/map/MapAnimation.js
+++ src/view/map/MapAnimation.js
@@ -981,7 +981,6 @@ MapAnimation.prototype.initStaticInteractions = function (interactionOptions) {
   interactionOptions['pinchRotate'] = false
   interactionOptions['pinchZoom'] = false
   interactionOptions['altShiftDragRotate'] = false
-  document.getElementById(config['mapContainer']).style.pointerEvents = 'none'
 }
 
 /**

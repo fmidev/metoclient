8e7120507595b08d10a1ceae91addf042195392a
Jussi Liukkonen <jussi.liukkonen@fmi.fi>
2019-05-14 12:43:43 +0300

    Small improvement for TimeSliderMenu

--- src/utils.js
+++ src/utils.js
@@ -162,7 +162,7 @@ export const createTimeMenu = (menuTimeSteps, options) => {
         title.innerHTML = item.title
         a = document.createElement('input')
         a.setAttribute('type', 'range')
-        a.setAttribute('min', '1')
+        a.setAttribute('min', '0')
         a.setAttribute('max', item.size)
         output = document.createElement('output')
         if (item.id === 'fmi-metoclient-timeslider-begintime') {
